d9fd9577689f8531ef430a634aae0b44917e5341
Isaias Cordova P <igerardo0808@gmail.com>
2023-12-05 02:02:48 -0600

    Modficando la clase agregarUsuario

--- src/componentes/AgregarUsuario.jsx
+++ src/componentes/AgregarUsuario.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
 function AgregarUsuario() {
+  const agregarUsuario = () => {
+    console.log("Agregando el usuario");
+  };
+
   return (
-    <div>
-      <h2>Agregar usuario</h2>
+    <div className="container">
+      <div className="row">
+        <h2 className="mt-4">Crear un nuevo usuario</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-6 offset-3">
+          <div className="mb-3">
+            <label htmlFor="nombre" className="form-label">
+              Nombre
+            </label>
+            <input type="text" className="form-control"></input>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input type="text" className="form-control"></input>
+
+            <div className="mb-3">
+              <label htmlFor="telefono" className="form-label">
+                Telefono
+              </label>
+              <input type="text" className="form-control"></input>
+            </div>
+            {/*  */}
+
+            <button className="btn btn-success" onClick={agregarUsuario}>
+              Guardar usuario
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
